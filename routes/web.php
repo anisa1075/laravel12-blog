5231169901f1db8e5594109152194914a5a1f8ca
anisa1075 <anisa.nisa1075@gmail.com>
2025-09-21 13:06:38 +0700

    Model Posts

--- routes/web.php
+++ routes/web.php
@@ -1,67 +1,22 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
 });
 
 Route::get('/posts', function () {
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Anisa',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eius consectetur
-            laboriosam est debitis?
-            Fugit architecto itaque doloremque unde tempore, expedita dolor veritatis facere mollitia praesentium. Vitae
-            adipisci aliquid ipsum.',
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Anisa',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eius consectetur
-            laboriosam est debitis?
-            Fugit architecto itaque doloremque unde tempore, expedita dolor veritatis facere mollitia praesentium. Vitae
-            adipisci aliquid ipsum.',
-        ],
-    ];
+    $posts = Post::all();
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => 1,
-             'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Anisa',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eius consectetur
-            laboriosam est debitis?
-            Fugit architecto itaque doloremque unde tempore, expedita dolor veritatis facere mollitia praesentium. Vitae
-            adipisci aliquid ipsum.',
-        ],
-        [
-            'id' => 2,
-             'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Anisa',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eius consectetur
-            laboriosam est debitis?
-            Fugit architecto itaque doloremque unde tempore, expedita dolor veritatis facere mollitia praesentium. Vitae
-            adipisci aliquid ipsum.',
-        ],
-    ];
-    $posts = Arr::first($posts, function($post) use($slug) {
-        return $post['slug'] == $slug;
-    });
-
-    if(!$posts) abort(404);
-    return view('post', ['title' => 'Single Post', 'post' => $posts]);
+    $post = Post::find($slug);
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/about', function () {
