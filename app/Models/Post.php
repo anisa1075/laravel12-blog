cdba89169e24098860d9bff4781578400889913d
anisa1075 <anisa.nisa1075@gmail.com>
2025-09-22 22:28:11 +0700

    Eloquent Model extends

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,40 +1,9 @@
 <?php
 
 namespace App\Models;
-
-use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Anisa',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eius consectetur
-            laboriosam est debitis?
-            Fugit architecto itaque doloremque unde tempore, expedita dolor veritatis facere mollitia praesentium. Vitae
-            adipisci aliquid ipsum.',
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Anisa',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eius consectetur
-            laboriosam est debitis?
-            Fugit architecto itaque doloremque unde tempore, expedita dolor veritatis facere mollitia praesentium. Vitae
-            adipisci aliquid ipsum.',
-            ],
-        ];
-    }
 
-    public static function find($slug)
-    {
-        return Arr::first(static::all(), fn($post) => $post['slug'] == $slug) ?? abort(404);
-    }
 }
