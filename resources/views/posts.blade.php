05fa6fc866a7028981786c4a3d910c7f53adb2e7
anisa1075 <anisa.nisa1075@gmail.com>
2025-10-15 21:41:50 +0700

    Update UI Single Post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,20 +1,5 @@
 <x-layout :title="$title">
 
-    {{-- @foreach ($posts as $post)
-        <article class="py-8 max-w-screen-md border-b border-gray-300">
-            <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
-                <h2 class="mb-1 text-3xl tracking-tighter font-bold text-gray-900">{{ $post['title'] }}</h2>
-            </a>
-            <div class="text-base text-gray-500">
-                By <a href="/authors/{{ $post->author->username }}" class="hover:underline text-gray-900">{{ $post->author->name }}</a>
-                in <a href="/categories/{{ $post->category->slug }}" class="hover:underline text-gray-900">{{ $post->category->name }}</a> | 1 January 2025
-            </div>
-            <p class="my-4 font-light">
-                {{ Str::limit($post['body'], 100) }}
-            </p>
-            <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
-        </article>
-    @endforeach --}}
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div class="grid gap-8 lg:grid-cols-3 md:grid-cols-2">
             @foreach ($posts as $post)
